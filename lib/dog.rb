c7406e2e11655c5153e30fb595e3d8de2c047a7d
Kordell Parrish <parrishkordell@gmail.com>
2020-08-12 05:52:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,3 +20,21 @@
 end
 
 
+ class
+  
+   def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
